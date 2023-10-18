895513c37ed06ee35b56fc00f30973f6c9b642cb
Erik Slovak <erik.slovak@technologystudio.sk>
2023-10-18 13:23:39 +0200

    chore: add eslint check into jest and add missing packages

---
+++ __tests__/Tests/Lint.test.ts
@@ -0,0 +1,14 @@
+/**
+ * @Author: Rostislav Simonik <rostislav.simonik@technologystudio.sk>
+ * @Date:   2018-01-09T00:56:40+01:00
+ * @Copyright: Technology Studio
+**/
+
+import { ESLint } from 'eslint'
+
+test('shoud not have lint issues', async () => {
+  const eslint = new ESLint()
+  const results = await eslint.lintFiles(['test/**/*.ts'])
+  const isWithoutError = results.every((result) => result.errorCount === 0)
+  expect(isWithoutError).toBe(true)
+})
