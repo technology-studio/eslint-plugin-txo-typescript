5a5b9347b90576c8b1143de565dfc936875d7f20
Erik Slovak <erik.slovak@technologystudio.sk>
2023-11-14 15:37:46 +0100

    chore: update lint test

--- __tests__/Tests/Lint.test.ts
+++ __tests__/Tests/Lint.test.ts
@@ -1,14 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /**
  * @Author: Rostislav Simonik <rostislav.simonik@technologystudio.sk>
  * @Date:   2018-01-09T00:56:40+01:00
  * @Copyright: Technology Studio
 **/
 
-import { ESLint } from 'eslint'
+const { FlatESLint } = require('eslint/use-at-your-own-risk')
 
 test('shoud not have lint issues', async () => {
-  const eslint = new ESLint()
+  const eslint = new FlatESLint()
   const results = await eslint.lintFiles(['test/**/*.ts'])
-  const isWithoutError = results.every((result) => result.errorCount === 0)
+  const isWithoutError = results.every((result: { errorCount: number }) => result.errorCount === 0)
   expect(isWithoutError).toBe(true)
-})
+}, 20000)
